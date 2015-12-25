22ceb22ea795919393340dd028001d86c9a5f26c
daxiangpanda <313514820@qq.com>
2015-12-25 20:42:30 +0800

    奇怪的数不会做 canyousee有新的想法 每个数取模 然后模二值化 然后取值 然后对比

--- can you see.py
+++ can you see.py
@@ -1,62 +1,78 @@
-# encoding: utf-8
-
-# import Image
-# import ImageEnhance
-# import ImageFilter
-import sys
-from pytesser import *
-
-# # 二值化
-threshold = 140
-table = []
-for i in range(256):
-    if i < threshold:
-        table.append(0)
-    else:
-        table.append(1)
+# # encoding: utf-8
 #
-# #由于都是数字
-# #对于识别成字母的 采用该表进行修正
-rep={'O':'0',
-    'I':'1','L':'1',
-    'Z':'2',
-    'S':'8'
-    };
+# # import Image
+# # import ImageEnhance
+# # import ImageFilter
+# import sys
+# from pytesser import *
+#
+# # # 二值化
+# threshold = 140
+# table = []
+# for i in range(256):
+#     if i < threshold:
+#         table.append(0)
+#     else:
+#         table.append(1)
+# #
+# # #由于都是数字
+# # #对于识别成字母的 采用该表进行修正
+# rep={'O':'0',
+#     'I':'1','L':'1',
+#     'Z':'2',
+#     'S':'8'
+#     };
+#
+# def  getverify1(name):
+#
+#     #打开图片
+#     im = Image.open(name)
+#     #转化到亮度
+#     imgry = im.convert('L')
+#     imgry.save(r'E:\\BaiduYunDownload\\bmp.tar\\bmp\\'+'g'+name.split('\\')[-1])
+#     #二值化
+#     out = imgry.point(table,'1')
+#     out.save(r'E:\\BaiduYunDownload\\bmp.tar\\bmp\\'+'b'+name.split("\\")[-1])
+#     #识别
+#     text = image_to_string(out)
+#     #识别对吗
+#
+#     print text
+#     return text
+# getverify1(r'E:\BaiduYunDownload\bmp.tar\bmp\1.bmp')
+#
+# im = Image.open(r'E:\1.tif')
+# imgry = im.convert('L')
+# out = imgry.point(table,'1')
+# textcode = image_to_string(im)
+# print textcode
+#
+# def logo_watermark(img):
+#     '''
+#     添加一个图片水印,原理就是合并图层，用png比较好
+#     '''
+#     baseim = Image.new ("RGBA", (60, 30), (255, 255, 255))
+#     logoim = Image.open(img)
+#     bw, bh = baseim.size
+#     lw, lh = logoim.size
+#     baseim.paste(logoim, (10,10))
+#     baseim.save(r'E:\\BaiduYunDownload\\bmp.tar\\bmp\\'+'a.jpg','JPEG')
+#     print u'logo水印组合成功'
+# img = r'E:\BaiduYunDownload\bmp.tar\bmp\1.bmp'
+# logo_watermark(img)
+#
+from PIL import Image
 
-def  getverify1(name):
+box1 = (0,0,6,10)
+box2 = (10,0,16,10)
+box3 = (20,0,26,10)
+box4 = (30,0,36,10)
 
-    #打开图片
-    im = Image.open(name)
-    #转化到亮度
-    imgry = im.convert('L')
-    imgry.save(r'E:\\BaiduYunDownload\\bmp.tar\\bmp\\'+'g'+name.split('\\')[-1])
-    #二值化
-    out = imgry.point(table,'1')
-    out.save(r'E:\\BaiduYunDownload\\bmp.tar\\bmp\\'+'b'+name.split("\\")[-1])
-    #识别
-    text = image_to_string(out)
-    #识别对吗
-
-    print text
-    return text
-getverify1(r'E:\BaiduYunDownload\bmp.tar\bmp\1.bmp')
-
-im = Image.open(r'E:\1.tif')
-imgry = im.convert('L')
-out = imgry.point(table,'1')
-textcode = image_to_string(im)
-print textcode
-
-def logo_watermark(img):
-    '''
-    添加一个图片水印,原理就是合并图层，用png比较好
-    '''
-    baseim = Image.new ("RGBA", (60, 30), (255, 255, 255))
-    logoim = Image.open(img)
-    bw, bh = baseim.size
-    lw, lh = logoim.size
-    baseim.paste(logoim, (10,10))
-    baseim.save(r'E:\\BaiduYunDownload\\bmp.tar\\bmp\\'+'a.jpg','JPEG')
-    print u'logo水印组合成功'
-img = r'E:\BaiduYunDownload\bmp.tar\bmp\1.bmp'
-logo_watermark(img)
+for i in range(1,17):
+    path = 'E:\\shiyanba\\1225\\canyousee\\bmptest\\'+str(i)+".bmp"
+    # print path
+    im = Image.open(path)
+    im.crop(box1).save('E:\\shiyanba\\1225\\canyousee\\bmptest\\'+str(i)+'01'+".bmp")
+    im.crop(box2).save('E:\\shiyanba\\1225\\canyousee\\bmptest\\'+str(i)+'02'+".bmp")
+    im.crop(box3).save('E:\\shiyanba\\1225\\canyousee\\bmptest\\'+str(i)+'03'+".bmp")
+    im.crop(box4).save('E:\\shiyanba\\1225\\canyousee\\bmptest\\'+str(i)+'04'+".bmp")
--- strange num.py
+++ strange num.py
@@ -1,2 +1,41 @@
 #!/usr/bin/env python
 # encoding: utf-8
+
+#coding:utf-8
+
+import hashlib
+
+def md5(s):
+    return hashlib.md5(s).hexdigest()
+
+def evalCrossTotal(strMd5):
+    r = 0
+    for i in strMd5:
+        r += int("0x%s" % i, 16)
+    return r
+
+def encryptString(strString, strPasswd):
+    strPasswdMd5 = md5(strPasswd)
+    intMd5       = evalCrossTotal(strPasswdMd5)
+    print intMd5
+    r = []
+
+    for i in range(len(strString)):
+        r.append(
+            ord(strString[i]) + \
+            int("0x%s" % strPasswdMd5[i%32], 16) - \
+            intMd5
+        )
+        # print md5(strString[:(i+1)])[:16]
+        # print md5(str(intMd5))[:16]
+        intMd5 = evalCrossTotal(
+            md5(strString[:(i+1)])[:16] + \
+            md5(str(intMd5))[:16]
+        )
+        # print intMd5
+    # print r
+    return " ".join(map(lambda x: str(x), r))
+
+s = 'KEY'
+
+print encryptString(s,'19910')
